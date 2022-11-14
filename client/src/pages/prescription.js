c14b4a4a41b3246e8f3e720ebccc36861e1f2dc2
Naveen Satanarachchi <naveensatanarachchi@outlook.com>
2022-11-14 13:34:54 +1100

    row edit toggler implemented

--- client/src/pages/prescription.js
+++ client/src/pages/prescription.js
@@ -39,6 +39,15 @@ const PrescriptionPage = () => {
 		setRow(updatedRows);
 	};
 
+	const handleEdit = (rowId) => {
+		let currentRows = [...rows];
+		let rowEditIdx = currentRows.findIndex((v) => v.id === rowId);
+		let rowEditObj = { ...currentRows[rowEditIdx] };
+		rowEditObj.isEditing = true;
+		currentRows[rowEditIdx] = rowEditObj;
+		setRow(currentRows);
+	};
+
 	useEffect(() => {
 		axios
 			.get("/api/products", {
@@ -103,10 +112,10 @@ const PrescriptionPage = () => {
 							>
 								{
 									<Stack
-										spacing={2}
 										sx={{
 											padding: "2rem",
 											borderRadius: "1rem",
+											rowGap: "1rem",
 										}}
 									>
 										<Autocomplete
@@ -164,6 +173,7 @@ const PrescriptionPage = () => {
 															selectedProductObject.unitPrice,
 															givenQty
 														),
+														isEditing: false,
 													},
 												]);
 												handleClose();
@@ -175,34 +185,47 @@ const PrescriptionPage = () => {
 								}
 							</Popover>
 						</div>
-						<TableContainer component={Paper}>
-							<Table sx={{ minWidth: 700 }} aria-label="spanning table">
-								<TableHead>
-									<TableRow>
-										<TableCell align="center">Desc</TableCell>
-										<TableCell align="center">Dosage</TableCell>
-										<TableCell align="center">Qty.</TableCell>
-										<TableCell align="center">Unit Price (Rs.)</TableCell>
-										<TableCell align="center">Sum (Rs.)</TableCell>
-										<TableCell align="center">Actions</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{rows &&
-										rows.map((row, i) => {
+						{rows.length !== 0 && (
+							<TableContainer component={Paper}>
+								<Table sx={{ minWidth: 700 }} aria-label="spanning table">
+									<TableHead>
+										<TableRow>
+											<TableCell align="center">Desc</TableCell>
+											<TableCell align="center">Dosage</TableCell>
+											<TableCell align="center">Qty.</TableCell>
+											<TableCell align="center">Unit Price (Rs.)</TableCell>
+											<TableCell align="center">Sum (Rs.)</TableCell>
+											<TableCell align="center">Actions</TableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										{rows.map((row, i) => {
 											return (
 												<TableRow idx={row.id} key={i}>
 													<TableCell align="center">
 														{row.productName}
 													</TableCell>
 													<TableCell align="center">{row.dosage}</TableCell>
-													<TableCell align="center">{row.qty}</TableCell>
+													{row.isEditing ? (
+														<TableCell align="center">
+															<TextField type={"number"}></TextField>
+														</TableCell>
+													) : (
+														<TableCell align="center">{row.qty}</TableCell>
+													)}
+
 													<TableCell align="center">{row.unitPrice}</TableCell>
 													<TableCell align="center">{row.sum}</TableCell>
 													<TableCell align="center">
-														<IconButton>
-															<EditIcon></EditIcon>
-														</IconButton>
+														{row.isEditing ? (
+															<IconButton>
+																<DoneIcon></DoneIcon>
+															</IconButton>
+														) : (
+															<IconButton onClick={() => handleEdit(row.id)}>
+																<EditIcon></EditIcon>
+															</IconButton>
+														)}
 														<IconButton onClick={() => handleCancel(row.id)}>
 															<CloseIcon></CloseIcon>
 														</IconButton>
@@ -210,28 +233,29 @@ const PrescriptionPage = () => {
 												</TableRow>
 											);
 										})}
-									{rows.length !== 0 && (
-										<>
-											<TableRow>
-												<TableCell colSpan={3}></TableCell>
-												<TableCell
-													sx={{ fontWeight: "bold", fontSize: "1.2em" }}
-												>
-													Total
-												</TableCell>
-												<TableCell
-													sx={{ fontWeight: "bold", fontSize: "1.2em" }}
-													align="center"
-												>
-													{calcTotal(rows)}
-												</TableCell>
-												<TableCell></TableCell>
-											</TableRow>
-										</>
-									)}
-								</TableBody>
-							</Table>
-						</TableContainer>
+										{rows.length !== 0 && (
+											<>
+												<TableRow>
+													<TableCell colSpan={3}></TableCell>
+													<TableCell
+														sx={{ fontWeight: "bold", fontSize: "1.2em" }}
+													>
+														Total
+													</TableCell>
+													<TableCell
+														sx={{ fontWeight: "bold", fontSize: "1.2em" }}
+														align="center"
+													>
+														{calcTotal(rows)}
+													</TableCell>
+													<TableCell></TableCell>
+												</TableRow>
+											</>
+										)}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						)}
 					</>
 				) : (
 					<h3>Loading...</h3>
@@ -253,104 +277,5 @@ const calcTotal = (items) => {
 	});
 	return total.toFixed(2);
 };
-
-// const AddView = ({ Products }, Rows) => {
-// 	const [selectedProductName, setSelectedProductName] = useState(null);
-// 	const [selectedDosage, setSelectedDosage] = useState(null);
-// 	const [selectedProductObject, setSelectedProductObject] = useState({});
-// 	const [givenQty, setGivenQty] = useState(null);
-// 	const [sum, setSum] = useState(0);
-
-// 	useEffect(() => {
-// 		if (selectedProductName === null) return;
-
-// 		const found = Products.find((p) => p.productName === selectedProductName);
-// 		setSelectedProductObject(found);
-// 	}, [selectedProductName]);
-
-// 	useEffect(() => {
-// 		if (selectedProductName === null || givenQty === null) return;
-
-// 		setSum(
-// 			(Number(selectedProductObject.unitPrice) * Number(givenQty)).toFixed(2)
-// 		);
-// 	}, [selectedProductName, givenQty]);
-
-// 	return (
-// 		<>
-// 			<TableCell align="center">
-// 				<Autocomplete
-// 					disablePortal
-// 					id="combo-box-demo"
-// 					options={Products.map((product) => product.productName)}
-// 					sx={{ width: 200 }}
-// 					renderInput={(params) => <TextField {...params} label="Product" />}
-// 					onInputChange={(event, value) => setSelectedProductName(value)}
-// 				/>
-// 			</TableCell>
-// 			<TableCell align="center">
-// 				{selectedProductName && (
-// 					<Autocomplete
-// 						disablePortal
-// 						id="combo-box-demo"
-// 						options={Array.of(selectedProductObject.dosage)}
-// 						sx={{ width: 100 }}
-// 						onInputChange={(event, value) => setSelectedDosage(value)}
-// 						renderInput={(params) => <TextField {...params} label="Dosage" />}
-// 					/>
-// 				)}
-// 			</TableCell>
-// 			<TableCell align="center">
-// 				{selectedProductName && (
-// 					<TextField
-// 						sx={{ width: 100 }}
-// 						id="outlined-number"
-// 						label="Qty"
-// 						type="number"
-// 						InputLabelProps={{
-// 							shrink: true,
-// 						}}
-// 						onChange={(event) => setGivenQty(event.target.value)}
-// 					/>
-// 				)}
-// 			</TableCell>
-// 			<TableCell align="center">
-// 				{selectedProductName &&
-// 					Number(selectedProductObject.unitPrice).toFixed(2)}
-// 			</TableCell>
-// 			<TableCell align="center">
-// 				{selectedProductName && givenQty && sum}
-// 			</TableCell>
-// 			<TableCell>
-// 				<IconButton
-// 					aria-label="done"
-// 					size="medium"
-// 					color="success"
-// 					disabled={!selectedProductName || !selectedDosage || !givenQty}
-// 				>
-// 					<DoneIcon fontSize="inherit" color="success"></DoneIcon>
-// 				</IconButton>
-// 				<IconButton
-// 					aria-label="close"
-// 					size="medium"
-// 					color="error"
-// 					onClick={(event) => {
-// 						let idx = event.currentTarget.parentNode.parentNode.getAttribute(
-// 							"data-key"
-// 						);
-// 						console.log(Rows);
-// 						removeRow(Rows, idx);
-// 					}}
-// 				>
-// 					<CloseIcon fontSize="inherit" color="error"></CloseIcon>
-// 				</IconButton>
-// 			</TableCell>
-// 		</>
-// 	);
-// };
-
-// const removeRow = (rows, index) => {
-// 	rows.slice(index, 1);
-// };
 
 export default PrescriptionPage;
